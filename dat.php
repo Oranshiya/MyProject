59c6c6a867edacdbc350ae94ce62462c9fe6d094
Oranshiya <oranshiya@ideas2it.com>
2015-09-10 17:25:12 +0530

    day1.html sub file

---
+++ dat.php
@@ -0,0 +1,13 @@
+<?php
+$x="hai";
+$t=25;
+$y=15.09;
+$z=array("1",ajay,10.5);
+echo $x;echo"<br>";
+echo $t;echo"<br>";echo $y;echo"<br>";
+var_dump($x);echo"<br>";
+var_dump($t);echo"<br>";
+var_dump($z);echo"<br>";
+print_r($z);echo"<br>";
+print_r($t);
+?>
---
+++ pri.php
@@ -0,0 +1,8 @@
+<?php
+$t1="hello";
+$t2="world";
+echo"$t1,$t2,<br>";
+echo"$t1.$t2,<br>";
+print"$t1<br>";
+print"$t1.$t2<br>";
+?>
